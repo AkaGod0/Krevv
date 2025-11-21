c60dd3e248f53e98d6cbb8f25341628d172a7d93
AkaGod0 <akavigo01@gmail.com>
2025-11-21 21:35:03 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,7 +83,10 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-
+ <head>
+  <GoogleTagManager />
+</head>
+        
 
       <body className="bg-cream text-gray-800 flex flex-col min-h-screen">
 
@@ -100,10 +103,7 @@ export default async function RootLayout({
           )}
         </main>
         <Footer />
-        <head>
-  <GoogleTagManager />
-</head>
-        <body>
+        
         <GoogleTagManager.Body />
       </body>
     </html>
