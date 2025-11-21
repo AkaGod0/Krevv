736cd399471f3d67b04492fc014c9d66580229f8
AkaGod0 <akavigo01@gmail.com>
2025-11-21 21:45:39 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,9 +83,8 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
- <head>
-  <GoogleTagManager />
-</head>
+
+      
       <body className="bg-cream text-gray-800 flex flex-col min-h-screen">
 
         <ConditionalNavbar />
