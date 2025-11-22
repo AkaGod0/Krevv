9bb1ee146a21b140307799a7593411a2aed9b67d
AkaGod0 <akavigo01@gmail.com>
2025-11-22 10:07:58 +0100

    Wrap children with ClientWrapper in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -102,6 +102,7 @@ export default async function RootLayout({
         <Footer />
         <body>
         <GoogleTagManager.Body />
+        <ClientWrapper>{children}</ClientWrapper>
       </body>
     </html>
   );
