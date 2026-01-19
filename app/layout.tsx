55d0211c99cc1984728ab860d6d8a8a684393f78
AkaGod0 <akavigo01@gmail.com>
2026-01-19 14:01:50 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from '@/components/GTM';
 import "./globals.css";
 import ConditionalNavbar from "../components/ConditionalNavbar";
 import Footer from "../components/Footer";
@@ -9,21 +10,21 @@ import { AdminProvider } from "../app/admin/context/AdminContext"; // ✅ Add th
 
 export const metadata = {
   title: {
-    default: "krevv — Guide Designed for Digital & Remote workers.",
-    template: "%s | krevv",
+    default: "Marketplace for Modern Work, Verified Jobs & Trusted Talent",
+    template: "%s | Krevv",
   },
   icons: {
     icon: [
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/apple-touch-icon.png", sizes: "180x180" },
-      { url: "/favicon.ico" }
+      { url: "/favicon.ico", sizes: 'any' }
     ],
     apple: "/apple-touch-icon.png",
   },
   manifest: "/site.webmanifest",
   description:
-    "A guide built for remote professionals and creatives who want to thrive in a digital world.",
+    "Discover verified jobs, hire trusted talent, and access a reliable marketplace designed to make work simple, secure, and efficient for everyone.",
   keywords: [
     "blog",
     "stories",
@@ -44,9 +45,9 @@ export const metadata = {
   publisher: "Krevv",
   robots: { index: true, follow: true },
   openGraph: {
-    title: "krevv — Guide Designed for Digital & Remote workers",
+    title: "Marketplace for Modern Work, Verified Jobs & Trusted Talent",
     description:
-      "A guide built for remote professionals and creatives who want to thrive in a digital world.",
+      "Discover verified jobs, hire trusted talent, and access a reliable marketplace designed to make work simple, secure, and efficient for everyone.",
     url: "https://krevv.com",
     siteName: "krevv",
     images: [
@@ -62,9 +63,9 @@ export const metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "krevv — Guide Designed for Digital & Remote workers.",
+    title: "Marketplace for Modern Work, Verified Jobs & Trusted Talent",
     description:
-      "A guide built for remote professionals and creatives who want to thrive in a digital world.",
+      "Discover verified jobs, hire trusted talent, and access a reliable marketplace designed to make work simple, secure, and efficient for everyone.",
     creator: "@yourhandle",
     images: "/krevv.png",
   },
@@ -83,6 +84,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <GoogleTagManager.Head />
       <body className="bg-cream text-gray-800 flex flex-col min-h-screen">
         {/* ✅ Wrap with both AuthProvider and AdminProvider */}
         <AuthProvider>
@@ -103,6 +105,7 @@ export default async function RootLayout({
             <Footer />
           </AdminProvider>
         </AuthProvider>
+        <GoogleTagManager.Body />
       </body>
     </html>
   );
